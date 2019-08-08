988bf19a7417c42eca5dfdc2542a4cac9a775173
GYT0313 <guyongtao@qq.com>
2019-08-08 23:55:15 +0800

    SpringSecurity

--- spring-boot-security/src/main/java/com/gyt/security/config/MySecurityConfig.java
+++ spring-boot-security/src/main/java/com/gyt/security/config/MySecurityConfig.java
@@ -1,16 +1,64 @@
 package com.gyt.security.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 授权规则
+     * @param http
+     * @throws Exception
+     */
     @Override
-    public void configure(WebSecurity web) throws Exception {
-//        super.configure(web);
+    protected void configure(HttpSecurity http) throws Exception {
         // 定制请求的授权规则
         http.authorizeRequests().antMatchers("/").permitAll()
-                .antMatchers("")
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
+
+        // 开启自动配置登陆的功能
+        http.formLogin();
+        // 1、/login来到登录页
+        // 2、重定向到/login?error 登陆失败
+        // 3、。。。
+
+        // 开启自动配置的注销功能
+        // 注销成功来到首页
+        http.logout().logoutSuccessUrl("/");
+        // 1、访问/logout，清空session
+        // 2、返回登陆首页
+    }
+
+    /**
+     * 认证规则
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return s.equals(charSequence.toString());
+            }
+        })
+                .withUser("zhangsan").password("123456").roles("VIP1", "VIP2")
+                .and()
+                .withUser("lisi").password("123456").roles("VIP2", "VIP3")
+                .and()
+                .withUser("wangwu").password("123456").roles("VIP1", "VIP3");
+
     }
 }
